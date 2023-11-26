aa49c7f049797a115d03107f4c50a5fcffd02cc3
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-11-25 22:58:01 -0500

    Update Size.java

--- src/main/java/com/example/rupizza/Size.java
+++ src/main/java/com/example/rupizza/Size.java
@@ -1,5 +1,9 @@
 package com.example.rupizza;
 
+/**
+* Enum class representing the 3 pizza size types
+* @authors Pranav Gummaluri, Yasasvi Tallapaneni
+*/
 public enum Size {
     small,
     medium,
