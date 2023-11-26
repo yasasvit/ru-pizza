88c428392dfd6b9ec9bc6055ffe7df9389106462
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-11-25 22:44:38 -0500

    Update Pizza.java

--- src/main/java/com/example/rupizza/Pizza.java
+++ src/main/java/com/example/rupizza/Pizza.java
@@ -2,6 +2,10 @@ package com.example.rupizza;
 
 import java.util.ArrayList;
 
+/*
+* This class is an abstract class that represents a pizza and it's properties
+* @authors Yasasvi Tallapaneni, Pranav Gummaluri
+*/
 public abstract class Pizza {
     protected ArrayList<Topping> toppings;
     protected Size size;
