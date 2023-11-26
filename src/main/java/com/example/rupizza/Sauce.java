a8d4f8a19c73e17d8e31de4e940c2c1978228bcf
EKDrifter458 <98769437+EKDrifter458@users.noreply.github.com>
2023-11-25 22:57:41 -0500

    Update Sauce.java

--- src/main/java/com/example/rupizza/Sauce.java
+++ src/main/java/com/example/rupizza/Sauce.java
@@ -1,5 +1,9 @@
 package com.example.rupizza;
 
+/**
+* Enum class representing the 2 pizza sauce types
+* @authors Pranav Gummaluri, Yasasvi Tallapaneni
+*/
 public enum Sauce {
     alfredo,
     tomato
